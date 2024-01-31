6dc39bfe41b6f8c302ed47cded3bfc3eea26b69d
SamuelValentin974 <72002737+SamuelValentin974@users.noreply.github.com>
2024-01-31 14:16:14 +0400

    Sets lasers to false in game

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,5 @@
 extends Node3D
 
-var in_game : bool = false
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if in_game:
-		$XROrigin3D/LeftHand/FunctionPointer/Laser.visible = false
-		$XROrigin3D/RightHand/FunctionPointer/Laser.visible = false
-	else:
-		$XROrigin3D/LeftHand/FunctionPointer/Laser.visible = true
-		$XROrigin3D/RightHand/FunctionPointer/Laser.visible = true
-
 func set_in_game(ing):
-	in_game = ing
+		$XROrigin3D/LeftHand/FunctionPointer/Laser.visible = ing
+		$XROrigin3D/RightHand/FunctionPointer/Laser.visible = ing
